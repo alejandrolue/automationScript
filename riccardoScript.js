de51fef082f9736cbec6f12996549ff72ebc78c6
alejandroluthi <alejandro.luthi@gmail.com>
2024-02-26 20:22:17 +0100

    added description

--- riccardoScript.js
+++ riccardoScript.js
@@ -1,1 +1,14 @@
+// ==UserScript==
+// @name        riccardo
+// @namespace   riccardo
+// @match       *://example.org/*
+// @grant       none
+// @version     1.0
+// @author      Alejandro Lüthi
+// @require https://code.jquery.com/jquery-3.2.1.min.js
+// @require http://userscripts-mirror.org/scripts/source/107941.user.js
+// @grant GM_setValue
+// @grant GM_getValue
+// @description 26/02/2024, 20:21:07
+// ==/UserScript==
 
